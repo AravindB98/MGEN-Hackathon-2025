5b3d54869b41fb2ac02befc6b575ec9fdf17c8fd
Aravind Balaji <37304853+AravindB98@users.noreply.github.com>
2026-07-16 17:06:59 +0530

    Delete frontend directory

--- frontend/src/services/sessionService.js
+++
@@ -1,6 +0,0 @@
-import API from "./api";
-
-export const fetchConversationSummary = async (sessionId) => {
-  const response = await API.get(`/sessions/${sessionId}/getConversationSummary`);
-  return response.data;
-};
